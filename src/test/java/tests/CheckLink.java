84f1c83ab589efa663b6dd04cbfeb12ecadc08d3
Fedor Kolpakov <sv3-teodor@yandex.ru>
2023-02-23 17:24:41 +0300

    First release

---
+++ src/test/java/tests/CheckLink.java
@@ -0,0 +1,29 @@
+package tests;
+//возможность перехода на главную страницу яндекса и сервиса
+import org.junit.Assert;
+import org.junit.Test;
+import pages.HomePage;
+import utils.BaseTestClass;
+
+public class CheckLink extends BaseTestClass {
+    @Test //Тестируем переход на страницу яндекса(дзен)
+    public void testGoToYandex(){
+        Assert.assertTrue(new HomePage(driver)
+                .goToYandexHomePageClick()
+                .isYandexPageOpen());
+    }
+
+
+
+    @Test //Тестируем на главную страницу самоката.
+    public void testGoToScooterServiceHomePage(){
+        Assert.assertTrue(new HomePage(driver)
+                 .btnRentScooterTopClick() //Уходим на страницу с заказом
+                .goToScooterHomePage()
+                .getScooterHomePageWelcomeText()); //Проверяем, текст на главной странице виден.
+    }
+
+
+
+
+}
